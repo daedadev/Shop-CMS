e033dd8b8697af9e2164e2a7ed6b0d0197db1980
Daniel Moore <wron2@hotmail.com>
2022-03-12 17:30:48 -0800

    Added tailwind

---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function MainPage() {
+  return (
+    <section className="text-slate-300">
+      <section></section>
+      <section></section>
+    </section>
+  );
+}
