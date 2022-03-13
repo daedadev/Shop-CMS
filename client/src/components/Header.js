97fe319e48ae0ef94ac8e4031a3ee3bc778228ea
Daniel Moore <wron2@hotmail.com>
2022-03-12 22:48:31 -0800

    Changing some file locations

---
+++ client/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <section>Top Top</section>
+      <section>
+        <div>
+          <h1>section5</h1>
+        </div>
+        <div>
+          <h1>section5</h1>
+        </div>
+        <div>
+          <h1>section5</h1>
+        </div>
+        <div>
+          <h1>section5</h1>
+        </div>
+      </section>
+    </header>
+  );
+}
---
+++ client/src/components/SideBar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function SideBar() {
+  return (
+    <section>
+      <div>
+        <div>
+          <h1>section1</h1>
+        </div>
+        <div>
+          <h1>section2</h1>
+        </div>
+        <div>
+          <h1>section3</h1>
+        </div>
+        <div>
+          <h1>section4</h1>
+        </div>
+        <div>
+          <h1>section5</h1>
+        </div>
+      </div>
+    </section>
+  );
+}
