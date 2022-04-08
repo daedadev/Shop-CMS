6e67687321996a96cfda0cf604c4609b46972766
Daniel Moore <wron2@hotmail.com>
2022-04-08 04:35:11 -0700

    Trying again

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
   });
 }
 
